0b9a4b0e2b1dfaf566a812ce931cd852a97a6257
DenisIvanko <denis.ivanko11@gmail.com>
2016-09-19 11:36:41 +0300

    First

--- VisemeLabelsMaker/VisemeLabelsMaker.cpp
+++ VisemeLabelsMaker/VisemeLabelsMaker.cpp
@@ -6,6 +6,49 @@
 
 int main()
 {
-    return 0;
+	//const int len = 30;
+	//const int strings = 200;
+	//const char ch = '\n';
+	//char mass[len][strings];
+
+	//std::string INPUT_FILE_NAME = "C:\\Users\\denis\\OneDrive\\Документы\\Visual Studio 2015\\Projects\\VisemeLabelsMaker\\VisemeLabelsMaker\\Labels\\1\\001-essv_001.lab";
+
+	//std::ifstream labelFile(INPUT_FILE_NAME, std::ios::in | std::ios::binary);
+	//if (!labelFile) return 1;
+
+	//for (int r = 0; r<strings; r++)
+	//{
+	//	labelFile.getline(mass[r], len - 1, ch); //Считываем строки в массив
+	//	std::cout << "String " << r + 1 << " = " << mass[r] << std::endl; //Выводи строку из массива
+	//}
+	//labelFile.close();
+
+	std::string LINE;
+	
+	std::string OUTPUT_FILE_NAME = "C:\\Users\\denis\\OneDrive\\Документы\\Visual Studio 2015\\Projects\\VisemeLabelsMaker\\VisemeLabelsMaker\\Labels\\1\\V001-essv_001.lab";
+	std::string INPUT_FILE_NAME = "C:\\Users\\denis\\OneDrive\\Документы\\Visual Studio 2015\\Projects\\VisemeLabelsMaker\\VisemeLabelsMaker\\Labels\\1\\001-essv_001.lab";
+
+	std::ifstream labelFile(INPUT_FILE_NAME);
+	std::ofstream VlabelFile(OUTPUT_FILE_NAME);
+
+	if (labelFile.is_open())
+	{
+		if (VlabelFile.is_open())
+		{
+			while (labelFile.good())
+			{
+				getline(labelFile, LINE);
+
+				VlabelFile << LINE << std::endl;
+				std::cout << LINE << std::endl;
+			}
+			labelFile.close();
+		}
+		else std::cout << "Unable to open output file" << std::endl;
+	}
+	else std::cout << "Unable to open input file" << std::endl;
+    
+
+	return 0;
 }
 
